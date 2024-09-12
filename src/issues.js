70801ac54c8f8d1bbc311d8deb3a689cdd9eda8e
Aiden Mitchell <aiden@sublimesecurity.com>
2024-09-12 07:47:16 -0700

    Added more issues

--- src/issues.js
+++ src/issues.js
@@ -8,7 +8,7 @@ export const issues = [
       Green: { stance: "unknown", explanation: "Stance unavailable", source: "" }
     },
     {
-      issue: "Public healthcare",
+      issue: "Investing in public health",
       importance: 5,
       notes: "Healthcare is a critical issue in B.C., centering around maintaining and improving the public healthcare system. Key topics include wait times, drug coverage, and the balance between public and private healthcare options.",
       NDP: { stance: "unknown", explanation: "Future plans unclear", source: "" },
@@ -36,11 +36,11 @@ export const issues = [
       importance: 4,
       notes: "",
       NDP: { stance: "support", explanation: "Wants to get to net-zero", source: "https://www.ndp.ca/climate-action" },
-      Conservative: { stance: "mixed", explanation: "Against carbon tax, wants to expand LNG and build more pipelines", source: "https://www.conservativebc.ca/ideas" },
+      Conservative: { stance: "oppose", explanation: "Against carbon tax, wants to expand LNG and build more pipelines", source: "https://www.conservativebc.ca/ideas" },
       Green: { stance: "support", explanation: "It's the Green party", source: "https://bcgreens2024.ca/" }
     },
     {
-      issue: "Housing",
+      issue: "Increasing housing",
       importance: 5,
       notes: "With a housing crisis currently in play, British Columbians are looking to the government for support.",
       NDP: { stance: "support", explanation: "Historically, regulated short-term rentals and updated zoning rules; recently recommended fire code changes to support cheaper developments while maintaining safety", source: "https://www.bcndp.ca/latest/five-bold-steps-weve-taken-housing-2023" },
@@ -54,5 +54,37 @@ export const issues = [
       NDP: { stance: "support", explanation: "Made birth control free, wants to pay for IVF", source: "https://bc.ctvnews.ca/ndp-tries-to-make-abortion-b-c-election-issue-conservatives-push-back-1.7033140" },
       Conservative: { stance: "mixed", explanation: "Will not change existing policy", source: "https://bc.ctvnews.ca/ndp-tries-to-make-abortion-b-c-election-issue-conservatives-push-back-1.7033140" },
       Green: { stance: "support", explanation: "Wants to invest in better access to services", source: "https://www.bcgreens.ca/improve_access_to_reproductive_healthcare_services" }
+    },
+    {
+        issue: "Public transit",
+        importance: 3,
+        notes: "Investments in public transit are important to work towards climate goals and help sustain transportation in large urban centres. With Metro Vancouver's TransLink facing a funding deficit by the end of 2025, this election cycle will prove to be important for public transit.",
+        NDP: { stance: "support", explanation: "Brought in free transit for children 12 and under, future plans unclear", source: "" },
+        Conservative: { stance: "support", explanation: "Wants to audit TransLink, address overcrowded routes in Surrey, and work on TransLink's funding model", source: "https://www.conservativebc.ca/john_rustad_no_more_billion_dollar_bailouts_time_to_get_translink_back_on_track" },
+        Green: { stance: "support", explanation: "Wants to make transit free for everyone and invest more in BC Transit & TransLink", source: "https://vancouversun.com/news/bc-greens-free-transit" }
+    },
+    {
+        issue: "Decriminalizing drugs",
+        importance: 5,
+        notes: 'With the opioid crisis continuing to kill too many British Columbians, parties are considering different solutions to the issue.',
+        NDP: { stance: "unknown", explanation: "Decriminalized drugs, future plans unclear", source: "" },
+        Conservative: { stance: "oppose", explanation: 'Wants to recriminalize hard drugs', source: "https://www.conservativebc.ca/ideas" },
+        Green: { stance: "support", explanation: "Wants to scale up safe supply; supports decriminalization", source: "https://www.bcgreens.ca/opioid_crisis" }
+    },
+    {
+        issue: "Policing",
+        importance: 4,
+        notes: 'Policing is a hot topic, with some believing that we need more police to ensure public safety, while others believe investments in mental health can help.',
+        NDP: { stance: "unknown", explanation: "Forced Surrey to create and maintain its own police force, future plans unclear", source: "" },
+        Conservative: { stance: "support", explanation: 'Wants to invest more in police', source: "https://www.conservativebc.ca/ideas" },
+        Green: { stance: "unknown", explanation: "Stance unknown", source: "" }
+    },
+    {
+        issue: "LGBTQ+ Rights",
+        importance: 3,
+        notes: '',
+        NDP: { stance: "support", explanation: "Continued to support Trans Care BC, introduced legislation to make gender marker changes easier, future plans unclear", source: "" },
+        Conservative: { stance: "mixed", explanation: 'Wants to "remove ideology from the classroom", could mean cuts to sex education and support within schools; has not mentioned LGBTQ+ healthcare', source: "https://www.conservativebc.ca/ideas" },
+        Green: { stance: "support", explanation: 'Previously supported a ban on conversion "therapy"', source: "" }
     },
   ];
