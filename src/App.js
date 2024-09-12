97194eae67d51dae967097e916962e5fd7b5e395
Aiden Mitchell <aiden@sublimesecurity.com>
2024-09-11 19:22:41 -0700

    Adding icons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App p-4">
       <h1 className="text-2xl font-bold mb-1">2024 IssueTracker for British Columbia</h1>
-      <p className="mb-4">Tracking issues that matter most to British Columbians, and parties views on the issues.</p>
+      <p>Tracking issues that matter most to British Columbians, and parties views on the issues.</p>
+      <p className="mb-4 italic">Best viewed in landscape on mobile devices.</p>
       <PoliticalStanceTable />
     </div>
   );
